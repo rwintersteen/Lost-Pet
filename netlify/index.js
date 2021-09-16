b8e8fb472317dedede6c70654cfb9c0f23582950
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-15 20:34:45 -0700

    Initial pushes, added netlify successfully, beginning to add basic homepage elements

---
+++ netlify/index.js
@@ -0,0 +1,19 @@
+const serverless = require("serverless-http");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const connection = mongoose.connect("mongodb://localhost:27017/test");
+
+const lostPetSchema = new Schema({ author: String, body: String });
+const PetModel = mongoose.model("lostpetschema", lostPetSchema);
+
+exports.handler = async event => {
+    await connection;
+
+    const newPet = await PetModel.create(JSON.parse(event.body));
+    console.log("saved to db: ", newPet);
+    return {
+        statusCode: 201,
+        body: JSON.stringify(newPet),
+    };
+};
